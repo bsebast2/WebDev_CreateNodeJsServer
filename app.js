7932075a9ed51712102349bb404d722b0336a4d5
Bentic Sebastian <ben111.sebastian@gmail.com>
2021-02-17 14:44:13 -0500

    remove original statis webpages

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 const  http=require('http');
 const fs=require('fs');
 
-const port=8084;
+const port=8080;
 const host='localhost';
+
 const server=http.createServer((req,res)=>{
-    console.log('method',req.method);
-    console.log('url',req.url);
-    console.log('header',req.headers);
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/html');
+
+    let path='./';
+
+    if(req.url=== '/'){
+        path=path+ 'views/index.html';
+    }else if(req.url==='/contact'){
+        path=path + 'views/contact.html';
+    }else if(req.url==='/about'){
+        path=path+ 'views/about.html';
+    }
+    else{
+        res.statusCode=400;
+        path=path+ 'views/404.html';
+    }
+
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+
+    });
+    
+    
+    
+    
+    
     //res.statusCode=200;
     //res.setHeader('Content-Type', 'text/html');
     //res.write();
     //res.end();
 
 
-}
-
-);
+});
 
 server.listen(port,host,() =>{
     console.log('The server is running on port',port);
